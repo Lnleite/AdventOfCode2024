c9cc4987ada7e79d00f73a418cff96a99ad2d398
Lnleite <lleite@nextdoor.com>
2024-12-03 12:20:57 -0500

    Day 3 Part 2: Do's and Dont's

--- day3/code.js
+++ day3/code.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 function formatData(data) {
-  const decipheredArray = data.match(/mul\(\d+,\d+\)/g);
+  const decipheredArray = data.match(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g);
   return decipheredArray;
 }
 
@@ -12,10 +12,20 @@ function multiplication(num1, num2) {
 function uncorruptedMultiplier() {
   const readFile = fs.readFileSync("./input.txt", "utf8");
   // const readFile =
-  //   "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+  //   "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
   const decipheredArray = formatData(readFile);
+  let shouldMultiply = true;
 
   return decipheredArray.reduce((total, mul) => {
+    if (mul === "do()") {
+      shouldMultiply = true;
+      return total;
+    } else if (mul === "don't()") {
+      shouldMultiply = false;
+      return total;
+    }
+
+    if (!shouldMultiply) return total;
     const [num1, num2] = mul.match(/\d+/g);
     return (total += multiplication(num1, num2));
   }, 0);
