58468138f382577bc8fd0dfe3b84488e987d7b62
Lnleite <lleite@nextdoor.com>
2024-12-06 14:44:43 -0800

    Part 1: Calculate correct print order median

--- day5/code.js
+++ day5/code.js
@@ -1,43 +1,91 @@
 const fs = require("fs");
 
 function formatData(data) {
-  return data.split("\n");
+  const [rules, printOrder] = data.split("\n\n");
+
+  const rulesArray = rules.split("\n");
+  const printOrderArray = printOrder.split("\n");
+
+  const rulesObject = {};
+
+  rulesArray.forEach((rule) => {
+    const [firstRule, secondRule] = rule.split("|");
+
+    if (firstRule in rulesObject && "before" in rulesObject[firstRule]) {
+      rulesObject[firstRule].before.add(secondRule);
+    } else {
+      rulesObject[firstRule] = {
+        ...rulesObject[firstRule],
+        before: new Set([secondRule]),
+      };
+    }
+  });
+
+  return [rulesObject, printOrderArray];
 }
 
 function totalMiddleNumberCorrectPrintQueue(data) {
-  return 0;
+  const [rulesObject, printOrderArray] = formatData(data);
+  let total = 0;
+
+  printOrderArray.forEach((printOrder) => {
+    const printOrderArray = printOrder.split(",");
+    let failedOrder = false;
+
+    for (let i = 1; i < printOrderArray.length; i++) {
+      const order = printOrderArray[i];
+      const rules = rulesObject[order];
+
+      if ("before" in rules) {
+        for (let j = i - 1; j >= 0; j--) {
+          const currentOrder = printOrderArray[j];
+          if (rules.before.has(currentOrder)) {
+            i = printOrderArray.length;
+            j = -1;
+            failedOrder = true;
+          }
+        }
+      }
+    }
+
+    if (!failedOrder) {
+      total += Number(printOrderArray[Math.floor(printOrderArray.length / 2)]);
+    }
+  });
+
+  return total;
 }
 
 function main() {
-  // const readFile = fs.readFileSync("./input.txt", "utf8");
-  const readFile = `47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
+  const readFile = fs.readFileSync("./input.txt", "utf8");
+  //   const readFile = `47|53
+  // 97|13
+  // 97|61
+  // 97|47
+  // 75|29
+  // 61|13
+  // 75|53
+  // 29|13
+  // 97|29
+  // 53|29
+  // 61|53
+  // 97|53
+  // 61|29
+  // 47|13
+  // 75|47
+  // 97|75
+  // 47|61
+  // 75|61
+  // 47|29
+  // 75|13
+  // 53|13
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47`;
+  // 75,47,61,53,29
+  // 97,61,53,29,13
+  // 75,29,13
+  // 75,97,47,61,53
+  // 61,13,29
+  // 97,13,75,29,47`;
 
   console.log(totalMiddleNumberCorrectPrintQueue(readFile));
 }
