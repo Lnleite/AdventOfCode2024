c9b15031bb9fe2bc51cf2208eaceaf4ef5d58f2b
Lnleite <lleite@nextdoor.com>
2024-12-09 21:50:12 -0800

    Day 7 Part 1 : Correct equations total

--- day7/code.js
+++ day7/code.js
@@ -2,24 +2,76 @@ const { kMaxLength } = require("buffer");
 const fs = require("fs");
 
 function formatData(data) {
-  const formattedData = data;
+  const formattedData = data.split("\n").map((equation) => {
+    const [total, equationNums] = equation.split(": ");
+    const equationNumsArray = equationNums.split(" ").map((num) => Number(num));
+    return [Number(total), equationNumsArray];
+  });
 
   return formattedData;
 }
 
-function main() {
-  // const readFile = fs.readFileSync("./input.txt", "utf8");
-  const readFile = `190: 10 19
-3267: 81 40 27
-83: 17 5
-156: 15 6
-7290: 6 8 6 15
-161011: 16 10 13
-192: 17 8 14
-21037: 9 7 18 13
-292: 11 6 16 20`;
+function canNumsEqualTotal(currentIndex, currentTotal, needTotal, equationNum) {
+  if (currentTotal === needTotal && currentIndex === equationNum.length) {
+    return true;
+  }
 
-  console.log(uniqueStepsGuardMarching(readFile));
+  if (currentTotal > needTotal) {
+    return false;
+  }
+
+  if (currentIndex === equationNum.length) return false;
+
+  return (
+    canNumsEqualTotal(
+      currentIndex + 1,
+      currentTotal + equationNum[currentIndex],
+      needTotal,
+      equationNum
+    ) ||
+    canNumsEqualTotal(
+      currentIndex + 1,
+      currentTotal * equationNum[currentIndex],
+      needTotal,
+      equationNum
+    )
+  );
+}
+
+function correctEquations(data) {
+  const formattedData = formatData(data);
+  let equationTotals = 0;
+
+  formattedData.forEach((equation) => {
+    const [total, equationNum] = equation;
+    const canEqualTotal = canNumsEqualTotal(
+      1,
+      equationNum[0],
+      total,
+      equationNum
+    );
+
+    if (canEqualTotal) {
+      equationTotals += total;
+    }
+  });
+
+  return equationTotals;
+}
+
+function main() {
+  const readFile = fs.readFileSync("./input.txt", "utf8");
+  //   const readFile = `190: 10 19
+  // 3267: 81 40 27
+  // 83: 17 5
+  // 156: 15 6
+  // 7290: 6 8 6 15
+  // 161011: 16 10 13
+  // 192: 17 8 14
+  // 21037: 9 7 18 13
+  // 292: 11 6 16 20`;
+
+  console.log(correctEquations(readFile));
 }
 
 main();
